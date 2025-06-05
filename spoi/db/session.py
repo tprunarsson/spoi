1c2fcc0197ae878a63894d19cca7ce5a64e503ed
tprunarsson <tpr@hi.is>
2025-06-05 15:04:46 +0000

    .

--- spoi/db/session.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-# For SQLite; for Postgres: "postgresql://user:pass@localhost/dbname"
-DATABASE_URL = "sqlite:///spoi.sqlite"
-
-engine = create_engine(DATABASE_URL, echo=False)
-SessionLocal = sessionmaker(bind=engine)
-
